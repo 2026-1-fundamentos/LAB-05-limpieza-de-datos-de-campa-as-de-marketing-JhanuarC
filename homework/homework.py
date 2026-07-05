e5ae3d8d34750d78079837cb7ad4451506e201e7
Jhanuar <jhcastrol@unal.edu.co>
2026-07-05 16:52:17 -0500

    test_v1

--- homework/homework.py
+++ homework/homework.py
@@ -6,6 +6,9 @@ Escriba el codigo que ejecute la accion solicitada.
 
 
 def clean_campaign_data():
+    from pathlib import Path
+    import pandas as pd 
+    import zipfile
     """
     En esta tarea se le pide que limpie los datos de una campaña de
     marketing realizada por un banco, la cual tiene como fin la
@@ -46,11 +49,77 @@ def clean_campaign_data():
     - const_price_idx
     - eurobor_three_months
 
+    
 
-
+    Plan de accion
+    -Como acceder a los .zip
+    -Crear los csv con los formatos requeridos
     """
+    ruta_csv = Path(r"files\input")
+    lista_csv = []
 
-    return
+    for archivo in ruta_csv.rglob("*.zip"):
+       with zipfile.ZipFile(archivo, "r") as z:
+            #Nombre del csv
+            nombre_csv = z.namelist()[0]
+
+            with z.open(nombre_csv) as f:
+                df = pd.read_csv(f)
+                lista_csv.append(df)
+
+    df_csv = pd.concat(lista_csv,ignore_index=True)#df con todos los datos
+
+
+    client_df = df_csv[[
+        "client_id",
+        "age",
+        "job",
+        "marital",
+        "education",
+        "credit_default",
+        "mortgage",
+    ]].copy()
+
+    #Limpieza de datos de client_df
+    client_df["job"] = client_df["job"].str.replace(".", "", regex=False).str.replace("-", "_", regex=False)
+    client_df["education"] = client_df["education"].str.replace(".", "_", regex=False).replace("unknown", pd.NA)
+    client_df["credit_default"] = (client_df["credit_default"] == "yes").astype(int)
+    client_df["mortgage"] = (client_df["mortgage"] == "yes").astype(int)  
+
+    campaign_df = df_csv[[
+        "client_id", 
+        "number_contacts", 
+        "contact_duration", 
+        "previous_campaign_contacts", 
+        "previous_outcome", 
+        "campaign_outcome", 
+        "day", 
+        "month",
+    ]].copy()
+
+    #Limpieza de datos de campaign_df
+    campaign_df["previous_outcome"] = (campaign_df["previous_outcome"] == "success").astype(int)
+    campaign_df["campaign_outcome"] = (campaign_df["campaign_outcome"] == "yes").astype(int)
+    campaign_df["last_contact_date"] = pd.to_datetime(
+        campaign_df["day"].astype(str) + "-" + campaign_df["month"].astype(str) + "-2022", 
+        format="%d-%b-%Y"
+    ).dt.strftime("%Y-%m-%d")
+    campaign_df.drop(columns=["day", "month"], inplace=True)
+
+    economics_df = df_csv[[
+        "client_id", 
+        "cons_price_idx", 
+        "euribor_three_months",
+    ]].copy()
+
+    #Exportar los dataframes a csv
+    output_path = Path("files/output")
+    #Crear la carpeta si no existe
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    client_df.to_csv(output_path / "client.csv", index=False)
+    campaign_df.to_csv(output_path / "campaign.csv", index=False)
+    economics_df.to_csv(output_path / "economics.csv", index=False)
 
 
 if __name__ == "__main__":
